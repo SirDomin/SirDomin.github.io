e03d57cae13064cd0ba7dd3eaf43e850ac63f6cd
SirDomin <domin12wsx@gmail.com>
2017-10-16 14:09:38 +0100

    oll
    oll v.001

--- cfop/algs/f2l - Copy.js
+++ cfop/algs/f2l - Copy.js
@@ -1,43 +1,57 @@
-f2l = [
-    "R' FRF' RU' R' URU' R' U2RU' R')",
-    "y (U' L' ULU) y' (RU' R')",
-    " (URU' R' U') y (L' UL)",
-    "y (L' U' LUL' U' L)",
-    "RU' R' URU' R'",
-    "y (L' U' LU2) (L' ULU' L' U' L)",
-    " y (L' U' LUL' ULU2) y' (RUR')",
-    "RUR' U' RUR'",
-    " y (L' ULU' L' UL)",
-    "(RUR' U2) (RU' R' URUR')",
-    "(RUR' U') (RU' R' U2) y (L' U' L)",
-    " (RUR' U') (RUR' U') (RUR')",
-    " (RU' R' U) y (L' UL)",
-    "RU2R' U' RUR'",
-    "y (L' U' LU2) (L' U' LU) (L' U' L)",
-    " y (L' U2LUL' U' L)",
-    "RUR' U2RUR' U' RUR'",
-    "RU' R' U2 RUR'",
-    "y (U' L' U2 LU' L' U L)",
-    " y (L' ULU2L' U' L)",
-    " URU2R' URU' R'",
-    " y (UL' ULU2L' UL)",
-    "y (UL' U' LU') y' (RUR')",
-    " U' RU' R' URUR'",
-    " (RU' R' U2) y (L' U' L)",
-    " y (U' L' UL)",
-    "RU' R' URU' R' U2RU' R'",
-    " RUR'",
-    " y (UL' U2LU2L' UL)",
-    " y (UL' U' LU2L' UL)",
-    " U' RUR' URUR'",
-    "U' RU' R' U2RU' R'",
-    " (U' RUR' U) y ( L' U' L)",
-    "URU' R'",
-    " y (L' ULU' L' ULU2L' UL)",
-    "y (UL' ULU' L' U' L)",
-    "y (L' ULU2) y' (RUR')",
-    "U' RUR' U2RU' R'",
-    " y (UL' U' LU' L' U' L)",
-    " U' RU2R' U2RU' R'",
-    " y (L' U' L)",
+oll = [
+ RU2 R' U' RUR' U' RU' R' 
+(RU2 R2 U') (R2U') (R2U2R)
+ x' (RU' R' DRUR' D') 
+ R2D' RU2R'DRU2R
+ x' (RUR' DRU' R' D') 
+ L' U' LU' L' U2L
+ RUR' URU2R' 
+(RU2R2FR) (F' U2R' FRF')
+ LF' L' FU2FU' RU' R' F' 
+(RU2R2FRF' U2) M' URU' L'
+ (FRUR' U' F') (fRUR' U' f') 
+ R' U2FRUR' U' F2U2FR
+ MUR' F2RUL' U L2 l' 14 MU' LF2L' U' RU' R2 r
+ M (URUR' U') M2 (U RU' r') 
+ R' U' FURU' R' F' R
+ LUF' U' L' ULFL' 
+ FURU' R' F'
+ F' U' L' ULF 20 (L' U' LU' L' U) (LULF' L' F)
+ (RUR' URU') (R' U' R' FRF') 
+ (R' F' LF') ( L' FLF' L' F2R)
+ LFR' FRF' R' FRF2L' 
+ F' (L' U' LU) (L' U' LU) F
+ F (RUR' U') (RUR' U') F' 
+ (r' U) (r2U') (r2U') (r2Ur')
+ (lU') (l2U) (l2U) (l2U' l) 
+ L' U' L y' LF' L' ULFL'
+ RUR' y R' FRU' R' F' R 
+ (R' U' RU F) x' (R U' R' UD')
+ (R' U' R' F) (RF' U) R 
+ (RUR' U') (R' FRF')
+ F (RUR' U') F' 
+ (RU2R2U' RU' R' U2) (F R F')
+ F (URU' R' URU' R') F' 
+ (L' B' LU') (R' URU' R' URL' BL)
+ (R' U' RU' R' U) y' (R' URB) 
+ R' F2LFL' FR
+ L F2R' F' RF' L' 
+ x' (R' F2R2U' R' UR' F2R)
+ FR' F' RURU' R' 
+ LFR' FRF2L'
+ R' F' LF' L' F2R 
+ R' L2FL' FLF2L' FL' R
+ LR2F' RF' R' F2RF' L' R 
+ F' LF' L2ULUL' U' LF2
+ FR' FR2U' R' U' RUR' F2 
+ (R' FRF' R' FRF') (RU R' U' RUR')
+ (LF' L' FLF' L' F) (L' U' L UL' U' L) 
+ (L F' L' U' L U) (F U' L')
+ (R' FRUR' U') (F' UR) 
+ (LF' L' U' LFL') y' (R' UR)
+ (R' FRUR' F' R) y (LU' L') 
+ (L' B' L) (R' U' RU) (L' BL)
+ (RBR') (LUL' U') (RB' R') 
+ (RL' BLR') U2 (RL' BLR')
+ (RUR' U') r (R' URU' r')
 ]
