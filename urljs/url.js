e0eb6e85182d9c6b9a243ae110b6d2d4ac94b7d7
SirDomin <domin12wsx@gmail.com>
2017-10-20 22:07:36 +0100

    urljs_commit1
    sending variables through link with javascript

---
+++ urljs/url.js
@@ -0,0 +1,51 @@
+window["urljs_variables"]=[];
+urlVariable=function(name,value){
+    this.name=name;
+    this.value=value;
+    return this.name;
+}
+function setVariable(name_of_variable,value_of_variable){
+    
+    for(var i in window["urljs_variables"])
+        {
+            if(window["urljs_variables"][i].name==name_of_variable){
+                
+                window["urljs_variables"][i].value=value_of_variable;
+            }else{
+                window["urljs_variables"].push(new urlVariable(name_of_variable,value_of_variable));
+                
+            }
+        }
+    if(window["urljs_variables"].length==0)
+        window["urljs_variables"].push(new urlVariable(name_of_variable,value_of_variable));
+
+    return true;
+}
+function redirect(link,variables_to_send){
+    var tmp="";
+    for(var i in window["urljs_variables"])
+        {
+            for(var x=1;x<arguments.length;x++)
+                {
+                    
+                    if(arguments[x]==window["urljs_variables"][i].name)
+                        tmp+=window["urljs_variables"][i].name+"="+window["urljs_variables"][i].value+"&";
+                }
+        }
+    document.location.href=link+"?"+tmp;
+}
+
+
+function getVariable(variable_name){
+    
+    var urlVar = document.location.search.replace("?","");
+    urlVar = urlVar.split("&");
+    
+    for(var i = 0;i<urlVar.length;i++)
+        {   
+            if(urlVar[i].match(variable_name))return urlVar[i].split("=")[1];
+                else return undefined;
+        }
+    
+    //console.log(document.location.search.match("/"+variable_name+"/"))
+}
