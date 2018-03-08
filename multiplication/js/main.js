3e47c186716ad9fb68781270a546c701c8cbc82c
SirDomin <domin12wsx@gmail.com>
2018-03-08 14:03:19 +0000

    v2
    download possible

--- multiplication/js/main.js
+++ multiplication/js/main.js
@@ -14,17 +14,26 @@ y = canvas.h / 2;
 elements = 10;
 var multiplier = 2;
 angle = 360/elements;
-
+var renderDots = 1;
+var transparent = false;
+document.getElementById("renderdots").onchange = function(){
+    renderDots ^= 1;
+    refresh();
+};
+document.getElementById("transparent").onchange = function(){
+  transparent ^= 1;
+  refresh();
+};
 document.getElementById("multiplierSlider").oninput = function(){
     document.getElementById("multiplier").value = this.value
     multiplier = parseInt(this.value) ;
     refresh();
-}
+};
 document.getElementById("elementsSlider").oninput = function(){
     document.getElementById("elements").value = this.value
     elements = parseInt(this.value) ;
     refresh();
-}
+};
 
 document.getElementById("multiplier").onchange = function(){
   multiplier = parseInt(this.value) || (document.getElementById("multiplier").value = 1);
@@ -32,6 +41,7 @@ document.getElementById("multiplier").onchange = function(){
 
 };
 document.getElementById("elements").onchange = function(){
+
     elements = parseInt(this.value) || (document.getElementById("elements").value = 1);
     refresh();
 };
@@ -45,6 +55,8 @@ var refresh = function(){
     var lastX = startX-2;
     var lastY = startY-2;
     ctx.clearRect(0,0,canvas.w,canvas.h);
+    ctx.fillStyle = "white";
+    if(!transparent)ctx.fillRect(0,0,canvas.w,canvas.h);
     for (var i = 0; i < elements; i++) {
         ctx.beginPath();
         //ctx.moveTo(canvas.w / 2, canvas.h / 2);
@@ -53,7 +65,7 @@ var refresh = function(){
         lastX = x + r * Math.cos(i * 2 * Math.PI / elements)-2;
         lastY = y + r * Math.sin(i * 2 * Math.PI / elements)-2;
         ctx.fillStyle = "red";
-        ctx.fillRect(points[i].x-1,points[i].y-1,2,2);
+        if(renderDots)ctx.fillRect(points[i].x-1,points[i].y-1,2,2);
         ctx.lineWidth = 0.5;
         ctx.strokeStyle = 'black';
         ctx.stroke();
@@ -67,7 +79,14 @@ var refresh = function(){
     ctx.stroke();
 };
 
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
+document.getElementById("download").addEventListener("click", function(){
+   downloadCanvas(this,"canvas","Circle E"+elements+"M"+multiplier);
 
+});
 refresh();
 
 
