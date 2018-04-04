c098b7d92dc51b72c4dd02e0012a3fe8b334eeef
SirDomin <domin12wsx@gmail.com>
2018-04-04 02:30:49 +0100

    change

--- particle/js/Particle.js
+++ particle/js/Particle.js
@@ -4,9 +4,9 @@ var Particle = function(x,y,r,id){
     this.r = r;
     this.id = id;
     this.render = function(){
-        if(Math.sqrt(Math.pow(mouseX-points[this.id].x,2)+Math.pow(mouseY-points[this.id].y,2)) < 50){
-            this.x = points[this.id].x + random(-50,50);
-            this.y = points[this.id].y + random(-50,50);
+        if(Math.sqrt(Math.pow(mouseX-this.x,2)+Math.pow(mouseY-this.y,2)) < 100){
+            this.x += (2)*Math.cos(Math.atan2(mouseY - points[this.id].y, points[this.id].x - mouseX));
+            this.y += (2)*Math.sin(Math.atan2(mouseY - points[this.id].y, points[this.id].x - mouseX));
         }else{
             this.x += (0.5)*Math.cos(Math.atan2(this.y - points[this.id].y,points[this.id].x - this.x ));
             this.y += -(0.5)*Math.sin(Math.atan2(this.y - points[this.id].y ,points[this.id].x - this.x));
