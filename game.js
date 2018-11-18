37f597b3158f5be01c580aba1ab6e705282180ad
SirDomin <domin12wsx@gmail.com>
2018-11-18 10:21:56 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -53,10 +53,6 @@ spiderBossR.src = "boss_w_prawo.png";
 butterflyImage = new Image();
 butterflyImage.src = "motyl.png";
 
-//bgAudio = new Audio("dj_rafix_-_calm_in_grass_finel.mp3");
-
-    
-   
 
 //db
 
@@ -121,7 +117,7 @@ pause = false;
 document.addEventListener("touchstart",function(e){
     if(!started){
         started = true;
-        //bgAudio.play();
+
     }
     if(e.changedTouches[0].pageX < canvas.width / 2){ // dwa różne ify, jeden dzieli na połowę ruszanie, a drugi od lewej/prawej gracza
     //if(e.changedTouches[0].pageX < player.x + (player.w / 2)){
